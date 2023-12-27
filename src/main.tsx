1d5972738fcf970585d07f8b55ffec72dcf307d5
Kleavant James <james@sparknspur.com>
2023-12-27 21:15:37 +0800

    added data grid

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import CssBaseline from "@mui/material/CssBaseline";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import theme from "./theme.ts";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
@@ -16,15 +16,15 @@ import "@fontsource/ibm-plex-sans/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter basename="/">
-      <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
+      <HashRouter basename="/">
         <LocalizationProvider dateAdapter={AdapterDateFns}>
           <ProfileProvider>
             <CssBaseline />
             <App />
           </ProfileProvider>
         </LocalizationProvider>
-      </ThemeProvider>
-    </HashRouter>
+      </HashRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/Profiles.tsx
+++ src/pages/Profiles.tsx
@@ -1,20 +1,36 @@
-import React, { Fragment } from 'react'
-import Grid from '@mui/material/Grid'
-import Paper from '@mui/material/Paper'
-import Typography from '@mui/material/Typography'
+import { Fragment } from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { useDemoData } from "@mui/x-data-grid-generator";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 
-function Profiles() {
+const Profiles = () => {
+  const { data } = useDemoData({
+    dataSet: "Employee",
+    rowLength: 100,
+  });
+
   return (
     <Fragment>
-      <Grid container>
-        <Grid item xs={12} sx={{ m: 3}}>
-          <Paper variant='outlined' sx={{ p: { xs: 2, md: 3 } }}>
-            <Typography>Test</Typography>
+      <Grid>
+        <Grid item xs={12} sx={{ m: 3 }}>
+          <Paper variant="outlined" sx={{ p: { xs: 2, md: 3 } }}>
+            <DataGrid
+              {...data}
+              density="comfortable"
+              disableDensitySelector
+              initialState={{
+                ...data.initialState,
+                pagination: { paginationModel: { pageSize: 10 } },
+              }}
+              pageSizeOptions={[10, 25, 50]}
+              slots={{ toolbar: GridToolbar }}
+            />
           </Paper>
         </Grid>
       </Grid>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Profiles
+export default Profiles;
