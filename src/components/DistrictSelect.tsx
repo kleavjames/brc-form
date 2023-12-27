2d3e81e47fb78ba93ea2944bd4d4efb6a85bd30c
Kleavant James <james@sparknspur.com>
2023-12-27 14:27:41 +0800

    removed unused code

--- src/components/DistrictSelect.tsx
+++ src/components/DistrictSelect.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import { useCallback } from "react";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
